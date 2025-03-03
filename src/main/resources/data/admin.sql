40dc50d7bea243d239efe1ca223444c2d0450b80
Kristina <kristina.beneva.hristova@gmail.com>
2025-03-03 14:48:07 -0500

    Ajout des donnees

---
+++ src/main/resources/data/admin.sql
@@ -0,0 +1,6 @@
+insert into admin (id, fname, lname, email, phone, password, isAdmin)
+values (1, 'Kenny', 'Carrick', 'kcarrick0@epa.gov', '490-922-3063', 'lU9,AqAM', true),
+       (2, 'Barbie', 'Cona', 'bcona1@com.com', '796-794-8956', 'mS9*+eAL*0RoH', true),
+       (3, 'Marla', 'Mumford', 'mmumford2@blinklist.com', '130-628-4773', 'xV0%qjYa0n4Tbm', true),
+       (4, 'Federico', 'Comizzoli', 'fcomizzoli3@hc360.com', '606-762-1085', 'yO4,twZ?', true),
+       (5, 'Marnia', 'Kleisel', 'mkleisel4@people.com.cn', '750-469-7451', 'xH3=Ss>$xSW', true);
---
+++ src/main/resources/data/customer.sql
@@ -0,0 +1,6 @@
+insert into customer (id, fname, lname, email, phone, password)
+values (11, 'Yehudi', 'Vittery', 'yvitterya@nsw.gov.au', '969-600-0719', 'rO7#G/f?a}KS'),
+       (12, 'Hilly', 'Aldis', 'haldisb@so-net.ne.jp', '122-539-1680', 'xM7(#bC<l('),
+       (13, 'Yale', 'Sansome', 'ysansomec@e-recht24.de', '493-559-0493', 'mM7%#wnCsNjO=kjY'),
+       (14, 'Gilbertine', 'Collopy', 'gcollopyd@ebay.com', '446-319-2807', 'fS1+?Bp,wPPpB'),
+       (15, 'Carmella', 'le Keux', 'clekeuxe@twitpic.com', '752-880-7347', 'aC1&NVk_0V{@m');
